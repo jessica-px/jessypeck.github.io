3e2d067cef3fb6a07da5cfcc991d33adafe4998e
Jessica Peck <jessypeck@gmail.com>
2018-02-11 23:27:03 -0800

    Ajax GET works, + new JS randomizer

--- coffee-orders/main.js
+++ coffee-orders/main.js
@@ -1,24 +1,79 @@
 // https://jessypeck.github.io/coffee-orders/orders.json
 
-var $orderList = $('#orderList'); // id of target ul in html
+var $orderList = $('#orderList'); // ul where orders are shown
+var $nameInput = $('#nameInput'); // ul where orders are shown
+var $drinkInput = $('#drinkInput'); // ul where orders are shown
+
+var names = ['Alice','Betty','Carlos','Daisuke','Enrqiue','Felicity',
+'Greta','Horatio','Ingrid','Junko','Karl','Luna','Momoko','Nora','Oscar','Petra',
+'John','Pete','Alex','Christina','Alison','Sarah','Veronica','Archie'];
+
+var drinks = ['Mocha','Chai Tea','Green Tea','Black Tea','Oolong Tea',
+'Latte','Chai Latte','Caramel Frappe','White Mocha','Americano','Espresso',
+'Cocoa','Skinny Latte','Iced Tea','Coffee','Cold Brewed Coffee'];
+
 
 $(function(){
-
 // Get Orders
     $.ajax({
         type: 'GET',
         url: 'https://jessypeck.github.io/coffee-orders/orders.json',
         success: function(orders){
             $.each(orders, function(i, order){
-                addOrderToList(order);    
+                addOrderToList(order.name, order.drink);    
             });
 
+        },
+        error: function(){
+            altert("Error, problem getting orders.");
         }
     })
 })
 
-function addOrderToList(order){
-    var newListEntry = "<li>Name: " + order.name + " , Order: " + order.drink + "</li>";
+//On Click "Submit" Button
+$('#addOrderBtn').on('click', function(){
+    console.log("Click!");
+    var newOrder = submitOrder();
+    addOrderToList(newOrder.name, newOrder.drink);
+});
+
+// Makes new Order from info in submit boxes
+function submitOrder(){
+    var newOrder = {
+        name : $nameInput.val(),
+        drink : $drinkInput.val()
+    };
+    return newOrder;
+}
+
+// Post Order to Database
+// NOT ACTIVE, need API ////////////
+function postOrder(newOrder){
+    $.ajax({
+        type: 'POST',
+        url: 'https://jessypeck.github.io/coffee-orders/orders.json',
+        data: newOrder,
+        success: addOrderToList(newOrder.name.stringify(), newOrder.drink),
+        error: function(){
+            altert("Error, problem submiting order.");
+        }
+    });
+}
+
+// Adds order info to Html unordered list
+function addOrderToList(name, drink){
+    var newListEntry = "<li>Name: " + name + ", Order: " + drink + "</li>";
     $orderList.append(newListEntry);
+}
+
+//On Click "New Order" Button
+$('#rndOrderBtn').on('click', function(){
+    addRandomOrder();
+});
+
+function addRandomOrder(){
+    var newName = names[Math.floor(Math.random()*names.length)];
+    var newDrink = drinks[Math.floor(Math.random()*drinks.length)];
+    addOrderToList(newName, newDrink);
 }
 
